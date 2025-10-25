ede0db6c690980715a04931d93ea0799ca2d355c
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2025-10-25 19:16:05 +0000

    update monthly budget shcemas

--- api/db/schemas/monthlyBudget.ts
+++ api/db/schemas/monthlyBudget.ts
@@ -1,34 +1,34 @@
 import { relations } from "drizzle-orm";
 import {
-  numeric,
-  pgTable,
-  timestamp,
-  uuid,
-  varchar,
+    numeric,
+    pgTable,
+    timestamp,
+    integer,
+    serial,
+    varchar,
 } from "drizzle-orm/pg-core";
 import { userTable } from "./user";
 
 export const monthlyBudgetTable = pgTable("MonthlyBudget", {
-  id: uuid().defaultRandom().primaryKey(),
-  month: varchar({ length: 15 }).notNull(),
-  year: numeric().notNull(),
-  limit_value: numeric().notNull(),
-  spent_value: numeric().default("0").notNull(),
-  created_at: timestamp().defaultNow().notNull(),
-  updated_at: timestamp()
-    .defaultNow()
-    .$onUpdate(() => new Date()),
-  user_id: uuid()
-    .notNull()
-    .references(() => userTable.id),
+    id: serial('id').primaryKey(),
+    month: varchar('month', { length: 15 }).notNull(),
+    year: integer('year').notNull(),
+    limit_value: numeric('limit_value').notNull(),
+    spent_value: numeric('spent_value').default("0").notNull(),
+    created_at: timestamp('created_at').defaultNow().notNull(),
+    updated_at: timestamp('updated_at')
+        .$onUpdate(() => new Date()),
+    user_id: integer('user_id')
+        .notNull()
+        .references(() => userTable.id),
 });
 
 export const monthlyBudgetRelations = relations(
-  monthlyBudgetTable,
-  ({ one }) => ({
-    user: one(userTable, {
-      fields: [monthlyBudgetTable.user_id],
-      references: [userTable.id],
-    }),
-  })
+    monthlyBudgetTable,
+    ({ one }) => ({
+        user: one(userTable, {
+            fields: [monthlyBudgetTable.user_id],
+            references: [userTable.id],
+        }),
+    })
 );
