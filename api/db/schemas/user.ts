ae11bc699980dba54d1dd6ceef03c791ce7c1de5
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2025-10-25 19:00:22 +0000

    update user schemas

--- api/db/schemas/user.ts
+++ api/db/schemas/user.ts
@@ -1,22 +1,22 @@
 import { relations } from "drizzle-orm";
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, varchar, serial } from "drizzle-orm/pg-core";
 import { accountTable } from "./account";
 import { financialGoalTable } from "./financialGoal";
 import { monthlyBudgetTable } from "./monthlyBudget";
 
 export const userTable = pgTable("User", {
-  id: uuid().defaultRandom().primaryKey(),
-  name: varchar().notNull(),
-  email: varchar().unique().notNull(),
-  password: varchar().notNull(),
-  created_at: timestamp().defaultNow().notNull(),
-  updated_at: timestamp()
-    .defaultNow()
-    .$onUpdate(() => new Date()),
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 100 }).notNull(),
+    email: varchar('email', { length: 100 }).unique().notNull(),
+    password: varchar('password', { length: 255 }).notNull(),
+    created_at: timestamp('created_at').defaultNow().notNull(),
+    updated_at: timestamp('updated_at')
+        .defaultNow()
+        .$onUpdate(() => new Date()),
 });
 
 export const userRelations = relations(userTable, ({ many }) => ({
-  account: many(accountTable),
-  monthlyBugdget: many(monthlyBudgetTable),
-  financialGoal: many(financialGoalTable),
+    account: many(accountTable),
+    monthlyBugdget: many(monthlyBudgetTable),
+    financialGoal: many(financialGoalTable),
 }));
