884c6a6eeb44e7f2a9819953ae9dc915781150a0
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2025-10-25 19:24:50 +0000

    return user schemas to uuid

--- api/db/schemas/user.ts
+++ api/db/schemas/user.ts
@@ -1,17 +1,16 @@
 import { relations } from "drizzle-orm";
-import { pgTable, timestamp, varchar, serial } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
 import { accountTable } from "./account";
 import { financialGoalTable } from "./financialGoal";
 import { monthlyBudgetTable } from "./monthlyBudget";
 
 export const userTable = pgTable("User", {
-    id: serial('id').primaryKey(),
+    id: uuid('id').defaultRandom().primaryKey(),
     name: varchar('name', { length: 100 }).notNull(),
     email: varchar('email', { length: 100 }).unique().notNull(),
     password: varchar('password', { length: 255 }).notNull(),
     created_at: timestamp('created_at').defaultNow().notNull(),
     updated_at: timestamp('updated_at')
-        .defaultNow()
         .$onUpdate(() => new Date()),
 });
 
