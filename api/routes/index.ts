043ea1968d3a274986e625b45bbf793c66e9e255
M-msdias <ddiasmarcela@gmail.com>
2025-11-07 00:42:04 +0000

    Finshed financialGoal entity(DTO, service, controller and router)

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,5 +1,6 @@
 import account from "./accountRouter.js";
 import auth from "./authenticationRouter.js";
 import user from "./userRoutes.js";
+import financialGoal from "./financialGoalRouter.js"
 
-export default { user, account, auth };
+export default { user, account, auth, financialGoal };
