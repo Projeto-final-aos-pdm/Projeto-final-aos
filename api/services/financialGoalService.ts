2472c14c69df270eba2ddb25dc9ef8b4a4d716fb
M-msdias <ddiasmarcela@gmail.com>
2025-11-07 00:25:37 +0000

    Create financialGoalService

---
+++ api/services/financialGoalService.ts
@@ -0,0 +1,43 @@
+import { eq } from "drizzle-orm";
+import { database } from "../db/index.js";
+import { financialGoalTable } from "../db/schemas/financialGoal.js";
+import type { FinancialGoalDTO } from "../dto/financialGoalDTO.js";
+
+const getAllFinalcialGoalsService = async () => {
+  return await database.query.financialGoalTable.findMany();
+};
+
+const getFinancialGoalByIdService = async (financialGoalId: string) => {
+  return await database.query.financialGoalTable.findFirst({
+    where: eq(financialGoalTable.id, financialGoalId),
+  });
+};
+
+const createFinancialGoalService = async (data: FinancialGoalDTO) => {
+  return await database.insert(financialGoalTable).values(data).returning();
+};
+
+const updateFinancialGoalByIdService = async (
+  financialGoalId: string,
+  data: Partial<FinancialGoalDTO>
+) => {
+  return await database
+    .update(financialGoalTable)
+    .set(data)
+    .where(eq(financialGoalTable.id, financialGoalId))
+    .returning();
+};
+
+const deleteFinancialGoalService = async (financialGoalId: string) => {
+  return await database
+    .delete(financialGoalTable)
+    .where(eq(financialGoalTable.id, financialGoalId));
+};
+
+export {
+  createFinancialGoalService,
+  deleteFinancialGoalService,
+  getAllFinalcialGoalsService,
+  getFinancialGoalByIdService,
+  updateFinancialGoalByIdService,
+};
