c30753d5c6735debd8ae59aa85156d030df418e0
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-11-16 16:26:35 -0300

    fix(spelling): correção das mensagens de erro

--- api/dto/financialGoalDTO.ts
+++ api/dto/financialGoalDTO.ts
@@ -2,9 +2,9 @@ import z from "zod";
 
 export const financialGoalDTO = z.object({
     description: z.string().nonempty(),
-    target_value: z.coerce.number().positive("The target valeu mut be bigger 0").transform(String),
-    current_value: z.coerce.number().min(0, "The current value cannot be negative").default(0).transform(String),
-    deadline: z.coerce.date().min(new Date, "The deadline must be in the future"),
+    target_value: z.coerce.number().positive("The target valeu must be bigger than 0.").transform(String),
+    current_value: z.coerce.number().min(0, "The current value cannot be negative.").default(0).transform(String),
+    deadline: z.coerce.date().min(new Date, "The deadline must be in the future."),
     user_id: z.string().uuid()
 })
 
